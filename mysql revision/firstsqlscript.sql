0cd559cb37628b318eb3d44ba94cfeb1e09e9f46
Roshan Karki <roshankarki4956@gmail.com>
2026-06-10 21:10:59 +0545

    Finishing the view section of the elarning

--- mysql revision/firstsqlscript.sql
+++ mysql revision/firstsqlscript.sql
@@ -127,4 +127,9 @@ select * from users where salary> 70000;
 */
 
 # update users set salary=30000 where id=2;
-select * from rich_users;
+#select * from rich_users;
+
+# drop view rich_users;
+
+
+
